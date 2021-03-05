02554145f660e3af6cfb56c96e156ab88a13d372
Sofia Safira <paudapps@gmail.com>
2021-03-05 16:21:07 +0700

    update auth test 4

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,7 @@ import Providers from 'next-auth/providers';
 export default NextAuth({
     providers: [
         Providers.Google({
+            site: 'https://server-test-netc.netcount.id' || 'http://localhost: 3000',
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code'
