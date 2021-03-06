afec4db27976ddabd0c9cf3b64adede99dfc05f6
Sofia Safira <paudapps@gmail.com>
2021-03-06 13:48:34 +0700

    update pwa 1

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = withPWA({
+    future: { webpack5: true },
     pwa: {
         disable: !isProd,
         dest: "public"
